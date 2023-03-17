ee7035738003cde0db2f000a527cd8c68937d7ad
Dmitriy Potter <dmitriypottero@gmail.com>
2023-03-17 14:25:29 +0500

    removed old build script

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-cmake -S .. -B production -DCMAKE_BUILD_TYPE=RELEASE -G "MinGW Makefiles"
-cmake -S .. -B debug -DCMAKE_BUILD_TYPE=DEBUG -G "MinGW Makefiles"    
-make -C production
-make -C debug
-mv production/chess.exe ../bin/production.exe
-mv debug/chess.exe ../bin/debug.exe
