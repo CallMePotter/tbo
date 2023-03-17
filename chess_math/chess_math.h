321e6cc7142ebea225d776539b6b1a3a83b281f5
Your Name <you@example.com>
2023-03-17 07:21:44 -0400

    fixing structure

--- chess_math/chess_math.h
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-#include <iostream>
-
-using namespace std;
-
-
-bool isCheckByBishop(int kingX, int kingY, int bishopX, int bishopY);
-bool isCheckByRook(int kingX, int kingY, int rookX, int rookY);
-void drawBoard(int kingX, int kingY, int bishopX, int bishopY, int rookX, int rookY);
